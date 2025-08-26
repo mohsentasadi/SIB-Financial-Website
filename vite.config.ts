70d0e767182476a3c0f600c79e05159958b3fda3
mohsentasadi <mohsen.t.asadi@gmail.com>
2025-08-27 01:26:38 +0330

    https://accountone.ir/
    removed:
    	base: '/SIB-Financial-Website/',
    
    changed:
    	base: '/',

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-	base: '/SIB-Financial-Website/',
+	base: '/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
